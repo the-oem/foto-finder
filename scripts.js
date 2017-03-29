dc637f09871197f5feb3ab715dceae9d6cac2e5b
Jason Collins <the.oem@gmail.com>
2017-03-28 22:46:53 -0600

    Add functionality to delete an imagecard from the album.

--- scripts.js
+++ scripts.js
@@ -8,20 +8,14 @@ function ImageCard(title, caption, imageName) {
 	this.caption = caption;
 	this.imageName = imageName;
 	this.favorite = function () {
+
 		// This is the method for toggling favorite status.
 	}
 	console.log(this);
 }
 
-// Function to delete an image. It needs to remove an image from the page.
-function deleteImage() {
-
-}
-
-// Event listener on the Choose File button
-
 // Event listener on the Add To Album button
-addToAlbumInput.addEventListener('click', function () {
+$(".save").on("click", function () {
 	var tempTitle = $('.title').val();
 	var tempCaption = $('.caption').val();
 	var tempFileName = getFilename($('.choose-file').val());
@@ -38,7 +32,13 @@ function getFilename(pathToImage) {
 
 // Function to add an image to the album (page)
 function addToAlbum(anImageCard) {
-	$(".cardset").prepend("<div class='card'><h6>" + anImageCard.title + "</h6><img src='' alt=''><p>" + anImageCard.caption + "</p><div class='card-actions'><img class='card-action' src='images/delete.svg' alt='delete'><img class='card-action' src='images/favorite.svg' alt='favorite'></div></div>");
+	$(".cardset").prepend("<div class='card'><h6>" + anImageCard.title + "</h6><img src='photos/" + anImageCard.imageName + "' alt='' width='250px'><p>" + anImageCard.caption + "</p><div class='card-actions'><img id='delete-image' class='card-action' src='images/delete.svg' alt='delete'><img id='favorite-image' class='card-action' src='images/favorite.svg' alt='favorite'></div></div>");
 }
+
+// Attach a delegated event handler to the parent container of the image cards
+$(".cardset").on("click", "#delete-image", function (event) {
+	console.log($(this));
+	$(this).closest(".card").remove();
+});
 
 // TODO PHASE 3: Event listener to make sure the user has a valid text title and caption, and that they have selected an image file. If all those conditions don't exist, do not enable the Save button.
